1362f7a4eee4b6d210f13adf779ab60aa194d01e
ppanorth <80310698+ppanorth@users.noreply.github.com>
2021-06-18 10:28:36 +0800

    Update push.js

--- push.js
+++ push.js
@@ -1,8 +1,8 @@
 var push = require('web-push');
 
 let vapidKeys = {
-       publicKey: 'BAEAgCGkkZWzcNxlrEsKXcnfmAjdqPsBo29_88n1sjTqYVc8Nq1c4KuypkqlJjRLquyWgfHrZ76bvVa9Fp-5I5Y',
-      privateKey: 'FmHDAB2XeOdrbkGImUOOmX-sVPuKdvFjzDrO_VnJqpY'
+  publicKey: 'BCtaF_7WQnr-r5R85Cx2-4SZ03MmaZE-sgUHUd_N63_5D9NgRddJvh-kCGlQ_31bYLsIA3LCkN4id9DLq3sT4eg',
+  privateKey: 't7rw-Wuz6tukAwMPOXO4Im6LUMaJ9fptqVu6Bh8OqhY'
 
 }
 
@@ -11,7 +11,7 @@ push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.pr
 let sub={
   endpoint:
   'https://fcm.googleapis.com/fcm/send/db7GVSmny4I:APA91bFCJyMDyiLTRVUP064PRjPq3qZZyrsB-wsSksVhjXAZcEn8n824iZHUnDHgPNkzgBjQQEPf4OZerKnkObfJ0D2vxYcF4pDk8E0vgDbjENKaXUX9LeWGG9ipkrZ23qknxlAAKK-s',
-  expirationTime:null,
+  expirationTime: null,
   keys:{
     p256dh:'BPJQsg2qG-PIXNBIj4w0uCQAMIMA1S5QNq_P6S-VywZXbrnl_CYIIg_ROrTpljwxFHdUYfO7k0v_RgXsqPwdTWs',
   auth:'iGvZKNQmaduW3EQWOvDrrA'
@@ -19,33 +19,3 @@ let sub={
 };
 
 push.sendNotification(sub, 'test message');
-
-
-
-
-
-
-// const webpush = require('web-push');
-
-// // VAPID keys should only be generated only once.
-// const vapidKeys = {
-//   publicKey: 'BBKEFJBTih20pCkFKXw1Gd51joIpavUXG1HrEES5IDvCa6mElW13QzQDuK4cC_aWP0zXjcZE5Wdhxyut1nCCzT0',
-//   privateKey: 'O5L6Bc7Dqa8Cd6LnquI3ezzWLlrlZ9rg3vEQyjhYfMs'
-// };
-
-// webpush.setVapidDetails(
-//   'mailto:example@yourdomain.org',
-//   vapidKeys.publicKey,
-//   vapidKeys.privateKey
-// );
-
-// // This is the same output of calling JSON.stringify on a PushSubscription
-// const pushSubscription = {
-//   endpoint: '.....',
-//   keys: {
-//     auth: '.....',
-//     p256dh: '.....'
-//   }
-// };
-
-// webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
